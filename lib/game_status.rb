eb212bddf41a193a6aaf98dc75a80d40f0815ac1
Mary Jack <maryjackinthebox@gmail.com>
2018-03-25 20:53:04 +0000

    automatically backed up by learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -28,12 +28,12 @@ def won?(board)
       position_1 = board[win_dex_1]  
       position_2 = board[win_dex_2]
       position_3 = board[win_dex_3]
+      
+        if (position_1 == "X" && position_2 == "X" && position_3 == "X") || (position_1 == "O" && position_2 == "O" && position_3 == "O") 
+            return win_combo
+        end #end of if
     end #end of do
   end # end of if/else
-      
-  if (position_1 == "X" && position_2 == "X" && position_3 == "X") || (position_1 == "O" && position_2 == "O" && position_3 == "O") 
-      return win_combo
-  end #end of if
 end # end of method 
 
 
